3f277fe42f77f6e300de2ff255f20848cf0e2e99
YinhuaChen-cloud <229676342@qq.com>
2026-04-28 01:56:24 +0800

    only cpu0 can run

--- src/start.rs
+++ src/start.rs
@@ -5,11 +5,18 @@ global_asm!(
     .section .boot.text, "ax"
     .global _start
 _start:
-    ldr x0, =__boot_stack_top
-    mov sp, x0
-    bl init
+    mrs x1, mpidr_el1
+    and x1, x1, #0xff
+    cbz x1, 0f
 1:
     wfe
     b 1b
+0:
+    ldr x0, =__boot_stack_top
+    mov sp, x0
+    bl init
+2:
+    wfe
+    b 2b
 "#
 );
